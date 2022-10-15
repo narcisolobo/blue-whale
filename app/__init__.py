ec141e04c1a3d2f13ef0e57324f942a75afbd563
narcisolobo <narcisolobo@gmail.com>
2022-10-15 16:16:23 -0700

    trying something else

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import os
+from dotenv import load_dotenv
 from flask import Flask, render_template, redirect, request, session, flash
 from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
+load_dotenv()
 app.secret_key = os.environ.get('SECRET_KEY')
